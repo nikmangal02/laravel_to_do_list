170bc78abf69a142ed0d1bfd1f1d84574cbedbb7
nikhilmangal02 <nikmangal@gmail.com>
2016-12-06 15:00:05 +0530

    final laravel project

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -54,6 +54,12 @@ class HomeController extends Controller
        $sum = DB::table('item')
            ->select(DB::raw('SUM(price) as sprice'),'item.store_id as stid')
            ->groupBy('store_id')->get();
+        foreach($sum as $stors)
+        {
+
+            DB::update('update store set price =  ? where id = ? ',array($stors->sprice,$stors->stid));
+
+        }
         $store_name = DB::table('store')->orderBy('price','desc')->get();
         $count_store_name = count($store_name);
         return view('welcome',compact('item_name','count' ,'count_store_name','name','sum','store_name'));
